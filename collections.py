b96c2c2b115f5624db3807e7a39a43a14c5842b1
DanielleRenee <danielle.r.russell@gmail.com>
2017-08-11 19:50:06 -0700

    first commit.

---
+++ collections.py
@@ -0,0 +1,10 @@
+course = {'title': 'Python Collections', 'student': 'Danielle'}
+
+course['student'] = 'Danielle'
+
+course = {'title': 'Python Collections', 'student': {'first name': 'Danielle', 'middle name': 'Renee'}}
+
+course['student']['first name'] = 'Danielle'
+
+
+
